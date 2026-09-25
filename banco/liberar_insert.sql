e04883e72a8482b53d3f5e0a13cbd22a827daa66
Calzavara-Dev <erickcalzav@gmail.com>
2026-09-24 22:16:09 -0300

    chore: remove banco folder from git tracking

--- banco/liberar_insert.sql
+++
@@ -1,4 +0,0 @@
--- Rode esse comando no SQL Editor do Supabase para autorizar o script Python a fazer envios para o banco
-CREATE POLICY "Permitir inserts publicos temporariamente"
-ON questoes FOR INSERT
-WITH CHECK (true);
